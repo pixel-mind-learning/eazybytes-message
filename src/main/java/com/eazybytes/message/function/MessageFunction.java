a49562ee71b5516bd2ce75c9279e4163e4978808
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-09-22 23:24:08 +0530

    creating two spring cloud functions

--- src/main/java/com/eazybytes/message/function/MessageFunction.java
+++ src/main/java/com/eazybytes/message/function/MessageFunction.java
@@ -1,9 +1,29 @@
 package com.eazybytes.message.function;
 
+import com.eazybytes.message.dto.AccountMsgDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.function.Function;
+
+@Slf4j
 @Configuration
 public class MessageFunction {
 
-    
+    @Bean
+    public Function<AccountMsgDTO, AccountMsgDTO> email() {
+        return accountMsgDTO -> {
+            log.info("Email sent to: {}", accountMsgDTO.toString());
+            return accountMsgDTO;
+        };
+    }
+
+    @Bean
+    public Function<AccountMsgDTO, Long> sms() {
+        return accountMsgDTO -> {
+            log.info("Sms sent to: {}", accountMsgDTO.toString());
+            return accountMsgDTO.accountNumber();
+        };
+    }
 }
